5a83e9ac31ce8bf4b7b738b7587af268040fbfa0
jergan-studio <jose343lopez22@gmail.com>
2026-06-11 13:00:40 -0500

    Add functions to navigate to game modes

---
+++ app.js
@@ -0,0 +1,9 @@
+function playLookalike() {
+  // stay inside launcher
+  window.location.href = "./game.html";
+}
+
+function playClassic() {
+  // external official Minecraft Classic
+  window.location.href = "https://classic.minecraft.net";
+}
